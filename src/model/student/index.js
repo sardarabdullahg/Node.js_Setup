97f5d699266126da534f63dfe76f0bee50414977
Sardar Abdullah <sardarabdullah511@gmail.com>
2024-01-28 18:39:35 +0500

    vh

--- src/model/student/index.js
+++ src/model/student/index.js
@@ -1,2 +1,14 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../config/database.js"
+import { sequelize } from "../config/database.js";
+
+const studentmodel = sequelize.define(
+  "NewStudent",
+  {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: { type: DataTypes.STRING, allowNull: false },
+      
+  },
+  {}
+);
+
+export default studentmodel;
